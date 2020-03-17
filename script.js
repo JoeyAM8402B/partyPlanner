c34b62d0109b5f2a3dc6739635d7c53bf449b1cf
Cynthia Yuen <cynthia.h.yuen@gmail.com>
2020-03-17 14:09:14 -0500

    started javascript to render user input to page

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.getElementById("start_button").onclick = function () {
-    location.href = "https://landing.html";
+// define variables
+var searchResults = document.getElementById("search-results");
+
+
+for (var i=0; i<results; i++){
+    var firstRequestDiv = document.createElement("div");
+    currentE1.textContent= (drink + " - $" + price);
+    drinkOptions.appendChild(currentE1);
 }
+
+searchButton.addEventListener("click", function(){
+    var newDrink = drinkOrder.value;
+    var newPrice = drinkPrice.value;
+    console.log(newDrink, newPrice);
+    var newItem = {drink: newDrink, price: newPrice};
+    drinkList.push(newItem);
+
+    var currentE1 = document.createElement("div");
+    currentE1.textContent= (newDrink + " - $" + newPrice);
+    drinkOptions.appendChild(currentE1);
+})
